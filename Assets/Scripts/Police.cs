9503594336a249e05e9ab1b002a9886ad1438466
Danvil <danvil@danvil.de>
2012-12-16 18:50:24 +0100

    Added more linq Tweaks

--- Assets/Scripts/Police.cs
+++ Assets/Scripts/Police.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using MoreLinq;
 
 public class Police : MonoBehaviour {
 	
@@ -12,7 +13,16 @@ public class Police : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		myself.FollowTarget = myself.ClosestEnemy;
-		myself.AttackTarget = myself.ClosestEnemy;
+		Person target = myself.PersonsInRange.Count == 0 ? null : myself.PersonsInRange.MinBy(x =>
+			(x.transform.position - myself.transform.position).magnitude
+			+ (x.faction==Faction.Police ? 1000.0f : (x.faction==Faction.Neutral ? 0.25f : 0.0f )));
+		if(target && target.faction != Faction.Police) {
+			myself.FollowTarget = target;
+			myself.AttackTarget = target;
+		}
+		else {
+			myself.FollowTarget = null;
+			myself.AttackTarget = null;
+		}
 	}
 }
