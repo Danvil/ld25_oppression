a9fc1fe01bd4d6877a28d2d6aa07e9257ffa0716
Danvil <danvil@danvil.de>
2012-12-16 14:22:50 +0100

    Added gates to buildings

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Building : MonoBehaviour {
 
+	public Vector3[] rawGates;
+
+	List<Vector3> gates = new List<Vector3>();
+
 	// Use this for initialization
 	void Start () {
-	
+		for(int i=0; i<rawGates.Length; i++) {
+			Vector3 pnt = this.transform.position + rawGates[i];
+			if(!Globals.City.IsBlocked(pnt)) {
+				gates.Add(pnt);
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach(Vector3 q in gates)
+			Debug.DrawLine(this.transform.position + new Vector3(0.5f,0,0.5f), q, Color.white);
 	}
 }
