c7b90309ed67bafa57c65882e39635affab4032b
Danvil <danvil@danvil.de>
2012-12-17 00:22:31 +0100

    Commanders are now moved with left click

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -16,7 +16,6 @@ public class Selector : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0)) {
 	        // Shoot ray from mouse position
 	        Ray ray = Globals.MainCamera.ScreenPointToRay(Input.mousePosition);
-			selected = null;
 	        RaycastHit[] hits = Physics.RaycastAll(ray);
 	        foreach(RaycastHit hit in hits) {
 				if(hit.transform.gameObject.layer == 8) {
@@ -24,39 +23,39 @@ public class Selector : MonoBehaviour {
 					Commander c = hit.transform.gameObject.GetComponent<Commander>();
 					if(c) {
 						selected = c;
+						return;
 					}
 				}
 	        }
 	    }
 			
+		// right click -> set commander goal
+		if(selected && Input.GetMouseButtonDown(0)) {
+			// http://answers.unity3d.com/questions/9737/how-can-i-make-the-character-follow-the-mouse-poin.html
+			
+		    // Generate a plane that intersects the transform's position with an upwards normal.
+		    Plane playerPlane = new Plane(Vector3.up, selected.transform.position);
+		
+		    // Generate a ray from the cursor position
+		    Ray ray = Globals.MainCamera.ScreenPointToRay(Input.mousePosition);
+		
+		    // Determine the point where the cursor ray intersects the plane.
+		    // This will be the point that the object must look towards to be looking at the mouse.
+		    // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+		    //   then find the point along that ray that meets that distance.  This will be the point
+		    //   to look at.
+		    float hitdist = 0.0f;
+		    // If the ray is parallel to the plane, Raycast will return false.
+		    if(playerPlane.Raycast(ray, out hitdist)) {
+		        // Get the point along the ray that hits the calculated distance.
+		       selected.Target = ray.GetPoint(hitdist);
+		    }
+		}
+		
 		this.gameObject.GetComponentInChildren<MeshRenderer>().enabled = selected;
 		if(selected) {
 			this.transform.position = selected.transform.position;
 		}
 		
-		// right click -> set commander goal
-		if(Input.GetMouseButtonDown(1)) {
-			if(selected) {
-				// http://answers.unity3d.com/questions/9737/how-can-i-make-the-character-follow-the-mouse-poin.html
-				
-			    // Generate a plane that intersects the transform's position with an upwards normal.
-			    Plane playerPlane = new Plane(Vector3.up, selected.transform.position);
-			
-			    // Generate a ray from the cursor position
-			    Ray ray = Globals.MainCamera.ScreenPointToRay(Input.mousePosition);
-			
-			    // Determine the point where the cursor ray intersects the plane.
-			    // This will be the point that the object must look towards to be looking at the mouse.
-			    // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
-			    //   then find the point along that ray that meets that distance.  This will be the point
-			    //   to look at.
-			    float hitdist = 0.0f;
-			    // If the ray is parallel to the plane, Raycast will return false.
-			    if(playerPlane.Raycast(ray, out hitdist)) {
-			        // Get the point along the ray that hits the calculated distance.
-			       selected.Target = ray.GetPoint(hitdist);
-			    }
-			}
-		}
 	}
 }
